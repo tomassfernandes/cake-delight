8f699adf54fb2d61ece00f1a2926291ceaf1aeb7
Tomás Fernandes <fernandesstomas@gmail.com>
2024-01-04 16:03:18 +0000

    scrollbar and delete item added to cart

--- cake-delight/src/Cart.js
+++ cake-delight/src/Cart.js
@@ -1,34 +1,75 @@
 import React from "react";
+import { useState } from "react";
 import { useCart } from "./CartContext";
 
 export default function Cart() {
-  const { cartState } = useCart();
+  const { cartState, dispatch } = useCart();
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const totalPrice = cartState.cartItems.reduce(
+    (total, item) => total + item.price,
+    0
+  );
+
+  const handleRemoveItem = (itemId) => {
+    // Dispatch the REMOVE_FROM_CART action with the item ID
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: { id: itemId },
+    });
+  };
+
+  const handleToggleCart = () => {
+    setCartVisible(!cartVisible);
+  };
 
   return (
-    <section className="cart-section">
+    <section className={`cart-section ${cartVisible ? "cart-visible" : ""}`}>
       <div className="cart-main-div">
         <div className="cart-heading-div">
           <h3 className="cart-heading-secondary">Shopping Cart</h3>
-          <span className="cart-close-btn">x</span>
+          <span className="cart-close-btn" onClick={handleToggleCart}>
+            x
+          </span>
         </div>
 
-        {cartState.cartItems.map((item) => (
-          <div key={item.id} className="cart-cake-item-div">
-            <div className="cart-cake-img-div">
-              {console.log(item)}
-              <img src={item.img} alt={item.name} className="cart-cake-img" />
-              <p className="cart-cake-name">{item.name}</p>
-            </div>
+        <div className="cart-items-container">
+          {cartState.cartItems.map((item) => (
+            <>
+              <div key={item.id} className="cart-cake-item-div">
+                <div className="cart-cake-img-div">
+                  {console.log(item)}
+                  <img
+                    src={item.img}
+                    alt={item.name}
+                    className="cart-cake-img"
+                  />
+                  <p className="cart-cake-name">{item.name}</p>
+                </div>
 
-            <div className="cart-adding-div">
-              <span className="cart-adding-text cart-adding-btn">-</span>
-              <p className="cart-adding-text">1</p>
-              <span className="cart-adding-text cart-adding-btn">+</span>
-            </div>
+                <div className="cart-adding-div">
+                  <span className="cart-adding-text cart-adding-btn">-</span>
+                  <p className="cart-adding-text">1</p>
+                  <span className="cart-adding-text cart-adding-btn">+</span>
+                </div>
 
-            <p className="cart-cake-price">{item.price}€</p>
-          </div>
-        ))}
+                <p className="cart-cake-price">{item.price}€</p>
+
+                <span
+                  className="delete-cake-item-btn"
+                  onClick={() => handleRemoveItem(item.id)}
+                >
+                  x
+                </span>
+              </div>
+              <div className="cart-seperator"></div>
+            </>
+          ))}
+        </div>
+      </div>
+      <div className="cart-checkout-div">
+        <p className="cart-checkout-text">{totalPrice}€</p>
+        <button className="cart-checkout-btn">CHECKOUT</button>
       </div>
     </section>
   );
