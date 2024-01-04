3c9154b3cc635da2a2f7c72d8b39f903fc690649
Tomás Fernandes <fernandesstomas@gmail.com>
2024-01-04 12:08:58 +0000

    second cart commit

--- cake-delight/src/Cart.js
+++ cake-delight/src/Cart.js
@@ -7,10 +7,17 @@ export default function Cart() {
   return (
     <section className="cart-section">
       <div className="cart-main-div">
+        <div className="cart-heading-div">
+          <h3 className="cart-heading-secondary">Shopping Cart</h3>
+          <span className="cart-close-btn">x</span>
+        </div>
+
         {cartState.cartItems.map((item) => (
-          <div key={item.id}>
-            <p className="cart-cake-name">{item.name}</p>
-            {/* Add more details or styling as needed */}
+          <div key={item.id} className="cart-cake-item-div">
+            <div className="cart-cake-img-div">
+              <img src={item.img} alt={item.name} className="cart-cake-img" />
+              <p className="cart-cake-name">{item.name}</p>
+            </div>
           </div>
         ))}
       </div>
